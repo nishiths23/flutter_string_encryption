fdf50915e715386dd626a0fda9c4880361fe12fa
nishith <nishithsingh23@ymail.com>
2022-10-02 18:49:03 +0530

    Removing example

--- android/src/main/kotlin/com/github/sroddy/flutterstringencryption/FlutterStringEncryptionPlugin.kt
+++ android/src/main/kotlin/com/github/sroddy/flutterstringencryption/FlutterStringEncryptionPlugin.kt
@@ -1,24 +1,31 @@
 package com.github.sroddy.flutterstringencryption
 
-import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.PluginRegistry.Registrar
-
+import androidx.annotation.NonNull
 import com.tozny.crypto.android.AesCbcWithIntegrity.*
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.BinaryMessenger
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.PluginRegistry
 import java.security.GeneralSecurityException
 
-class FlutterStringEncryptionPlugin(): MethodCallHandler {
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar): Unit {
-      val channel = MethodChannel(registrar.messenger(), "flutter_string_encryption")
-      channel.setMethodCallHandler(FlutterStringEncryptionPlugin())
-    }
+class FlutterStringEncryptionPlugin(): FlutterPlugin, MethodChannel.MethodCallHandler {
+  /// The MethodChannel that will the communication between Flutter and native Android
+  ///
+  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+  /// when the Flutter Engine is detached from the Activity
+  private lateinit var channel : MethodChannel
+
+  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_string_encryption")
+    channel.setMethodCallHandler(this)
   }
 
-  override fun onMethodCall(call: MethodCall, result: Result) {
+  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
+  }
+
+  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: MethodChannel.Result) {
     when (call.method) {
       "decrypt" -> {
         val data = call.argument<String>("data")
